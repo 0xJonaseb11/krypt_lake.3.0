8c7432f8707fb6e1a0fb77bb998b4cd2ab594322
jonas-sebera <sebejaz99@gmail.com>
2023-11-03 08:31:56 +0200

    bug-fix init

--- Smart_Contract/scripts/deploy.js
+++ Smart_Contract/scripts/deploy.js
@@ -21,31 +21,3 @@ const runMain = async () => {
 
 runMain();
 
-
-
-// // depploy lock
-// async function _main() {
-//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-//   const unlockTime = currentTimestampInSeconds + 60;
-
-//   const lockedAmount = ethers.parseEther("0.1");
-
-//   const lock = await ethers.deployContract("Lock", [unlockTime], {
-//     value: lockedAmount,
-//   });
-
-//   await lock.waitForDeployment();
-
-//   console.log(
-//     `Lock with ${ethers.formatEther(
-//       lockedAmount
-//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-//   );
-// }
-
-// // We recommend this pattern to be able to use async/await everywhere
-// // and properly handle errors.
-// _main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
