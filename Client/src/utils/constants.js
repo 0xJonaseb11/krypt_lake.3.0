98f3d3fe5b6deb6e4d2093edda90ef004ef1942a
jonas-sebera <sebejaz99@gmail.com>
2023-11-03 09:22:55 +0200

    init configuration

--- Client/src/utils/constants.js
+++ Client/src/utils/constants.js
@@ -2,5 +2,6 @@ import abi from "./Transactions.json";
 
 //Address gotten after running npx hardhat scripts/deploy.js --network <network_name>
 export const contractABI = abi.abi;
-export const contractAddress = "0xD3792A5E5B604C7CD44fEe52Ee606A56d5F29397"; 
+export const contractAddress = "0xE1D3427f90599FfE57755014AAfc7a77721539F7"; 
+
 
